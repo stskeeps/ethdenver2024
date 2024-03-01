1a200d8689d4f9a334f074baa57e474d4601409b
Milton Jonathan <milton.jonathan@cartesi.io>
2024-03-01 00:14:10 -0700

    Small run.sh improvements

--- run.sh
+++ run.sh
@@ -10,7 +10,21 @@ while [ ! -f /tmp/main.pid ]; do
 done
 sleep 1
 
-curl -sSL http://127.0.0.1:5004/get_tx
+curl -sSL http://127.0.0.1:5004/get_tx > /dev/null
+
+echo STARTING APP
 
 cd /opt/cartesi/dapp
 yarn start
+
+if [ $? != 0 ]; then
+  echo EXIT APP AS EXCEPTION
+  curl -X POST http://127.0.0.1:5004/exception
+else
+  echo EXIT APP AS FINISH
+  curl -X POST http://127.0.0.1:5004/finish
+fi
+
+sleep 1
+
+exit 0

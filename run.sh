788c4f152d44d10768abbc40da856075a9c3b14f
Danilo Tuler <tuler@pobox.com>
2024-03-01 10:21:29 -0700

    Forcing initial block for testing

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ sleep 1
 echo STARTING APP
 
 cd /opt/cartesi/dapp
-node dist/index.js
+node dist/index.js 0x5fd72d79165dc9940882a1b9bb415bed4af94a349fbfdbcc5aea9abd1a06c49b
 
 if [ $? != 0 ]; then
   echo EXIT APP AS EXCEPTION
